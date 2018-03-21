d0694c8aef5aae9880fc8c7dad2a1ad06a11fb1f
haltode <tallancon@gmail.com>
2018-03-21 17:24:39 +0100

    add license

--- src/action_deplacer.hh
+++ src/action_deplacer.hh
@@ -1,3 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+** This file is part of Prologin2018, a rules library for stechec2.
+**
+** Copyright (c) 2018 Association Prologin <info@prologin.org>
+**
+** Prologin2018 is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Prologin2018.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef ACTION_DEPLACER_HH
 #define ACTION_DEPLACER_HH
 
--- src/interface.cc
+++ src/interface.cc
@@ -1,13 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
-** Stechec project is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 2 of the License, or
-** (at your option) any later version.
+** This file is part of Prologin2018, a rules library for stechec2.
 **
-** The complete GNU General Public Licence Notice can be found as the
-** `NOTICE' file in the root directory.
+** Copyright (c) 2018 Association Prologin <info@prologin.org>
 **
-** Copyright (C) 2018 Prologin
+** Prologin2018 is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Prologin2018.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "api.hh"
