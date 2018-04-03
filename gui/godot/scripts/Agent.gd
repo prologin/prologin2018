4b6eaa26d7c4dda892e048192e240320857b808c
Sacha Delanoue <contact@shaac.me>
2018-04-03 22:04:48 +0200

    Godot: script for agent

---
+++ gui/godot/scripts/Agent.gd
@@ -0,0 +1,41 @@
+extends KinematicBody2D
+
+var modulate_color = Color(1, 1, 1, 1)
+var moving = false
+var _moving_to = Vector2()
+var _dash = false
+
+const SPEED = 100
+
+func set_team(blue):
+	if blue:
+		modulate_color = Color(0, 0.5, 1, 1)
+	else:
+		modulate_color = Color(1, 0.5, 0, 1)
+	set_modulate(modulate_color)
+
+func move_to(to, dash):
+	assert not moving
+	_moving_to = to
+	_dash = dash
+	moving = true
+	var anim = "slide" if _dash else "walk"
+	$AnimatedSprite.play(anim)
+	var dx = to.x - position.x
+	if dx > 0:
+		$AnimatedSprite.flip_h = false
+	elif dx < 0:
+		$AnimatedSprite.flip_h = true
+
+func _process(delta):
+	if moving:
+		var diff = _moving_to - position
+		if diff == Vector2():
+			$AnimatedSprite.play("idle")
+			moving = false
+		else:
+			var speed = SPEED * 2.5 if _dash else SPEED
+			if diff.length() > speed * delta:
+				move_and_slide(diff.normalized() * speed)
+			else:
+				move_and_slide(diff / delta)
--- gui/godot/scripts/TileMap.gd
+++ gui/godot/scripts/TileMap.gd
@@ -11,9 +11,6 @@ var agents = []
 var agents_pos = []
 
 var moving_agent = -1
-var moving_dir = Vector2()
-var dash = false
-const SPEED = 100
 
 const Util = preload("res://scripts/MapReader.gd")
 
@@ -26,20 +23,14 @@ func get_tile(x, y):
 	var tile = "Wall" if walls[x][y] else "BG"
 	return get_tileset().find_tile_by_name(tile)
 
-func spawn_agent(i):
-	var character = character_scene.instance()
-	character.position = world_position(agents_pos[i].x, agents_pos[i].y)
-	if (i < NB_AGENTS):
-		character.set_modulate(Color(0, 0.5, 1, 1))
-	else:
-		character.set_modulate(Color(1, 0.5, 0, 1))
-	add_child(character)
-	agents.append(character)
-
 func spawn_agents():
 	agents = []
 	for i in range(2 * NB_AGENTS):
-		spawn_agent(i)
+		var character = character_scene.instance()
+		character.position = world_position(agents_pos[i].x, agents_pos[i].y)
+		character.set_team(i < NB_AGENTS)
+		add_child(character)
+		agents.append(character)
 
 func set_map():
 	clear()
@@ -58,21 +49,14 @@ func move_agent(i, dir):
 	var dest = agents_pos[i] + dir
 	if not is_cell_free(dest):
 		return false
-	dash = false
+	var dash = false
 	if Input.is_action_pressed("ui_shift"):
 		dash = true
 		while is_cell_free(dest + dir):
 			dest += dir
 	moving_agent = i
-	moving_dir = dest
-	var anim = "slide" if dash else "walk"
-	var animated_sprite = agents[moving_agent].get_node("AnimatedSprite")
-	animated_sprite.play(anim)
-	var dx = dest.x - agents_pos[i].x
-	if dx > 0:
-		animated_sprite.flip_h = false
-	elif dx < 0:
-		animated_sprite.flip_h = true
+	agents[moving_agent].move_to(world_position(dest.x, dest.y), dash)
+	agents_pos[moving_agent] = dest
 	return true
 
 func _ready():
@@ -92,15 +76,5 @@ func _process(delta):
 			move_agent(1, Vector2(1, 0))
 		elif Input.is_action_just_pressed("ui_left"):
 			move_agent(1, Vector2(-1, 0))
-	if moving_agent != -1:
-		var diff = world_position(moving_dir.x, moving_dir.y) - agents[moving_agent].position
-		if diff == Vector2():
-			agents[moving_agent].get_node("AnimatedSprite").play("idle")
-			agents_pos[moving_agent] = moving_dir
-			moving_agent = -1
-		else:
-			var speed = SPEED * 2.5 if dash else SPEED
-			if diff.length() > speed * delta:
-				agents[moving_agent].move_and_slide(diff.normalized() * speed)
-			else:
-				agents[moving_agent].move_and_slide(diff / delta)
+	if moving_agent != -1 and not agents[moving_agent].moving:
+		moving_agent = -1
