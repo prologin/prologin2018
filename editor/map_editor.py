ed809cb3f0a80de63cde7a18da0a16f91d88a713
haltode <tallancon@gmail.com>
2018-04-14 13:52:51 +0200

    editor: add coordinates next to the grid

--- editor/map_editor.py
+++ editor/map_editor.py
@@ -8,12 +8,13 @@ MAP_CELL_SIZE = 30
 
 OUTPUT_FILENAME = "map_output"
 
+BORDER_COLOR = "black"
+EMPTY_COLOR = "white"
+FILLED_COLOR = "black"
+GRID_OFFSET = 50
+
 
 class Cell():
-    COLOR_BORDER = "black"
-    EMPTY_COLOR_BG = "white"
-    FILLED_COLOR_BG = "black"
-
     def __init__(self, master, x, y):
         self.master = master
         self.x = x
@@ -25,16 +26,16 @@ class Cell():
 
     def draw(self):
         if self.master is not None:
-            outline = Cell.COLOR_BORDER
+            outline = BORDER_COLOR
 
             if self.fill:
-                fill = Cell.FILLED_COLOR_BG
+                fill = FILLED_COLOR
             else:
-                fill = Cell.EMPTY_COLOR_BG
+                fill = EMPTY_COLOR
 
-            xmin = self.x * MAP_CELL_SIZE
+            xmin = self.x * MAP_CELL_SIZE + GRID_OFFSET
             xmax = xmin + MAP_CELL_SIZE
-            ymin = self.y * MAP_CELL_SIZE
+            ymin = self.y * MAP_CELL_SIZE + GRID_OFFSET
             ymax = ymin + MAP_CELL_SIZE
 
             self.master.create_rectangle(
@@ -44,8 +45,9 @@ class Cell():
 class Grid(Canvas):
     def __init__(self, master):
         Canvas.__init__(self, master,
-                        width=MAP_CELL_SIZE * MAP_NB_COL,
-                        height=MAP_CELL_SIZE * MAP_NB_ROW)
+                        width=MAP_CELL_SIZE * MAP_NB_COL + 2 * GRID_OFFSET,
+                        height=MAP_CELL_SIZE * MAP_NB_ROW + 2 * GRID_OFFSET)
+
         self.save_button = Button(master, text="Save", command=self.save_grid)
         self.save_button.pack()
 
@@ -69,12 +71,26 @@ class Grid(Canvas):
             for cell in row:
                 cell.draw()
 
+        for row in range(MAP_NB_ROW):
+            y_pos = MAP_CELL_SIZE * (row + 1) - (MAP_CELL_SIZE // 2)
+            self.create_text(30, y_pos + GRID_OFFSET, text=str(row + 1))
+        for col in range(MAP_NB_COL):
+            x_pos = MAP_CELL_SIZE * (col + 1) - (MAP_CELL_SIZE // 2)
+            self.create_text(x_pos + GRID_OFFSET, 30, text=str(col + 1))
+
     def get_coords(self, event):
-        row = int(event.y / MAP_CELL_SIZE)
-        col = int(event.x / MAP_CELL_SIZE)
+        row = int((event.y - GRID_OFFSET) / MAP_CELL_SIZE)
+        col = int((event.x - GRID_OFFSET) / MAP_CELL_SIZE)
         return row, col
 
+    @staticmethod
+    def inside_grid(event):
+        return GRID_OFFSET <= event.x < GRID_OFFSET + MAP_NB_ROW * MAP_CELL_SIZE and \
+               GRID_OFFSET <= event.y < GRID_OFFSET + MAP_NB_COL * MAP_CELL_SIZE
+
     def handle_mouse_click(self, event):
+        if not self.inside_grid(event):
+            return
         row, col = self.get_coords(event)
         cell = self.grid[row][col]
         cell.switch()
@@ -82,6 +98,8 @@ class Grid(Canvas):
         self.switched.append(cell)
 
     def handle_mouse_motion(self, event):
+        if not self.inside_grid(event):
+            return
         row, col = self.get_coords(event)
         cell = self.grid[row][col]
 
