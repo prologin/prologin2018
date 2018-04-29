f90ffb7c6d93484631670aa443c6b5cb743ad570
Sacha Delanoue <contact@shaac.me>
2018-04-29 23:53:50 +0200

    Godot: read alien info

--- gui/godot/scripts/DumpReader.gd
+++ gui/godot/scripts/DumpReader.gd
@@ -17,12 +17,20 @@ class PlayerStats:
 	var agents = []
 	var history = []
 
+class Alien:
+	var pos = Vector2()
+	var points = 0
+	var first_turn = 0
+	var duration = 0
+	var capture = 0
+
 class Turn:
 	var roundNumber = 0
 	var roundTotal = 0
 	var players = []
 	var map_size = 0
 	var walls = []
+	var aliens = []
 
 static func parse_turn(json):
 	var result = Turn.new()
@@ -46,4 +54,12 @@ static func parse_turn(json):
 		result.walls.append([])
 		for r in range(size):
 			result.walls[c].append(cells[r * size + c] == "MUR")
+	for alien_data in json["map"]["aliens"]:
+		var alien = Alien.new()
+		alien.pos = Vector2(alien_data["pos"]["c"], alien_data["pos"]["r"])
+		alien.points = alien_data["points_capture"]
+		alien.first_turn = alien_data["tour_invasion"]
+		alien.duration = alien_data["duree_invasion"]
+		alien.capture = alien_data["capture_en_cours"]
+		result.aliens.append(alien)
 	return result
